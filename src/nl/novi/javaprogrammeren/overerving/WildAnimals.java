3d791b95d0fd22a3ff181b81051fa142c4c59ed8
LuciaRyan <lcryan06@gmail.com>
2023-06-08 15:15:44 +0200

    extended class

--- src/nl/novi/javaprogrammeren/overerving/WildAnimals.java
+++ src/nl/novi/javaprogrammeren/overerving/WildAnimals.java
@@ -1,4 +1,17 @@
 package nl.novi.javaprogrammeren.overerving;
 
-public class WildAnimals {
+public class WildAnimals extends Animal {
+    String cageName;
+    String dayLastFed;
+    String countryOfOrigin;
+
+
+    public WildAnimals(String name, String gender, String cageName, String dayLastFed, String countryOfOrigin) {
+        super(name, gender);
+        this.cageName = cageName;
+        this.dayLastFed = dayLastFed;
+        this.countryOfOrigin = countryOfOrigin;
+    }
 }
+
+
