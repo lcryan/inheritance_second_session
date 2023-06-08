2dde47ea02a4459a841724c834b88c3e966b21bf
LuciaRyan <lcryan06@gmail.com>
2023-06-08 14:58:55 +0200

    corrected set up - all super and classes are set up correctly now

--- src/nl/novi/javaprogrammeren/AnimalMain.java
+++ src/nl/novi/javaprogrammeren/AnimalMain.java
@@ -1,7 +1,5 @@
 package nl.novi.javaprogrammeren;
 
-import nl.novi.javaprogrammeren.overerving.Lion;
-
 public class AnimalMain {
 
     public void main(String[] args) {
@@ -21,14 +19,25 @@ public class AnimalMain {
 
     1. Make 2 super classes : one for DomesticAnimals(dog, cat) and one for WildAnimals (lion, tiger, wolf)
     2. There are in total  5 animals - lion, tiger, wolf, dog and cat
-    2. All WILD animals have the following in common:  cageName, sex-gender, dayLastFed, countryOfOrigin
+    2. All WILD animals have the following in common: name, cageName, sex-gender, dayLastFed, countryOfOrigin
     3. All WILD animals also have one unique feature: lion - numberOfKids; tiger: numberOfStripes; wolf: packName.
     4. All DOMESTIC animals have the following in common: name, nameOfOwner, favouriteFoodBrand, type, gender.
+    5. ALL GENERAL animals have the following in common : gender, name.
     5. The cat has ONE special feature: houseCat or outsideCat.
 
     ALL animals have to be able to do the following :
     1. method one: be able to move : if they move then print in the console: 0.25 meters moved.
     2. method two: they have  to make sounds: print out the sort of sound in the console.
-    3.
+    3. method three : ALL animals eat - print out WHAT they eat
+    4.method four: ALL animals have to sleep  8 hours a day (boolean ? )
+  --> all of the methods will be added to the ANIMAL.JAVA!
 
+  Plan of steps:
+  1. Make all 3 super-classes - Animal, Wild Animals, Domestic Animals - DONE
+  2. Make all animals classes - DONE
+  3. Start to add variables and methods for ALL animals to Animal.java :
+
+  4.  Add variables for all DOMESTIC animals : nameOfOwner, favouriteFoodBrand, type.
+  5. Add variables to all WILD animals: cageName, sex-gender, dayLastFed, countryOfOrigin.
+  6.
     */
--- src/nl/novi/javaprogrammeren/overerving/Animal.java
+++ src/nl/novi/javaprogrammeren/overerving/Animal.java
@@ -1,17 +1,35 @@
 package nl.novi.javaprogrammeren.overerving;
 
-public abstract class Animal {
-    String animal;
-    String cageName;
-    String lastFed;
-    String weekday;
-    String gender;
+public class Animal {
+    final String name;
+    final String gender;
 
-    public Animal(String animal, String cageName, String lastFed, String weekday, String getLastFed, String gender) {
-        this.animal = animal;
-        this.cageName = cageName;
-        this.lastFed = lastFed;
-
+    public Animal(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
     }
 
+    public void animalMoved() {
+        boolean animalMoved = false;
+        if (animalMoved) {
+            System.out.println("The" + name + "animal has moved by 0.25 meters");
+        }
+    }
+
+    public void animalSound() {
+        System.out.println(name + "makes..."); //TODO: here add the sound of the animal - how to do this ???
+    }
+
+    public void eat() {
+        System.out.println(name + "ate..."); //TODO : add favourite food variable here! 
+    }
+
+    public void sleep() {
+        System.out.println(name + "slept....hours"); //TODO : try to add different amount of hours here!
+    }
 }
+
+
+// here you add the methods etc. p.p. that ALL animals have in common!!!
+
+//  TODO : variables: gender, name. Methods : move, sound, eat, sleep - set getters and setters for all! Decide what to make private and what to make public!
